f458e7ef642dc4673bd605d049a6f57ec02fbb38
Romain Bulcourt <rbulcourt@theyellowtree.fr>
2015-06-02 17:30:52 +0200

    publisher + debut book

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -1,19 +1,51 @@
 <?php
 use Demo\Author;
 use Demo\AuthorQuery;
+use Demo\Publisher;
+use Demo\PublisherQuery;
 	$app->get('/', function(){
 
 	});
-	$app->post('/sendAuthor', function() use ($app){
+
+	$app->post('/addAuthor', function() use ($app){
 		$post=$app->request->post();
 		$Author = new Author;
-		$Author->setFirstName($post['nom']);
-		$Author->setLastName('Toto');
+		$Author->setLastName($post['nom']);
+		$Author->setFirstName($post['prenom']);
 		$Author->save();
 		$authors = AuthorQuery::create()->find();
 		foreach ($authors as $aut) {
-			$aut->getFirstName();
 			echo "$aut";
 		};
 	});
+
+	$app->post('/delAuthor', function() use ($app){
+		$post=$app->request->post();
+		AuthorQuery::create()
+		  ->filterByLastName($post['nom'])
+		  ->filterByFirstName($post['prenom'])
+		  ->delete();
+		$authors = AuthorQuery::create()->find();
+		foreach ($authors as $aut) {
+			echo "$aut";
+		};
+	});
+
+	$app->post('/addPublisher', function() use ($app){
+		$post=$app->request->post();
+		$Publisher = new Publisher;
+		$Publisher->setName($post['nom']);
+		$Publisher->save();
+		$publishers = AuthorQuery::create()->find();
+		foreach ($publishers as $publi) {
+			echo "$publi";
+		};
+	});
+
+	$app->post('/addBook', function() use ($app){
+		$post=$app->request->post();
+		$Book = new Book;
+		
+	});
+
 ?>
--- public_html/index.php
+++ public_html/index.php
@@ -1,21 +1,4 @@
-<!doctype html>
-<html lang="fr">
-	<head>
-	 	<meta charset="utf-8">
-	 	<title>Route</title>	
-	</head>
-	<body>
-		<form action="sendAuthor" method="post">
-		    <div>
-		        <label for="nom">Nom :</label>
-		        <input type="text" id="nom" name="nom" />
-		    </div>   
-		    <div class="button">
-		        <button type="submit">Envoyer votre message</button>
-		    </div>
-		</form>
-	</body>
-</html>
+
 
 <?php
 	require '../vendor/autoload.php';
@@ -24,3 +7,108 @@
 	include('../app/routes/routes.php');
 	$app->run();
 ?>
+
+<?php 
+
+$authors = \Demo\AuthorQuery::create()
+	->find();
+
+
+$publishers = \Demo\PublisherQuery::create()
+	->find();
+
+?>
+
+<!doctype html>
+<html lang="fr">
+	<head>
+	 	<meta charset="utf-8">
+	 	<title>Route</title>	
+	</head>
+	<body>
+
+		<div id="addAuthor" style="border-style: solid;">
+			<label>Ajouter un auteur</label>
+			<form action="addAuthor" method="post">
+			    <div>
+			        <label for="nom">Nom :</label>
+			        <input type="text" id="nom" name="nom" />
+			    </div> 
+			    <div>
+			        <label for="prenom">Prenom :</label>
+			        <input type="text" id="prenom" name="prenom" />
+			    </div>  
+			    <div class="button">
+			        <button type="submit">Ajouter</button>
+			    </div>
+			</form>
+		</div>	
+
+		<br>
+
+		<div id="delAuteur" style="border-style: solid;">
+			<label>Supprimer un auteur</label>
+			<form action="delAuthor" method="post">
+			    <div>
+			        <label for="nom">Nom :</label>
+			        <input type="text" id="nom" name="nom" />
+			    </div> 
+			    <div>
+			        <label for="prenom">Prenom :</label>
+			        <input type="text" id="prenom" name="prenom" />
+			    </div>  
+			    <div class="button">
+			        <button type="submit">Supprimer</button>
+			    </div>
+			</form>
+		</div>
+
+		<br>
+
+		<div id="addPublisher" style="border-style: solid;">
+			<label>Ajouter un éditeur</label>
+			<form action="addPublisher" method="post">
+			    <div>
+			        <label for="nom">Nom :</label>
+			        <input type="text" id="nom" name="nom" />
+			    </div> 
+			    <div class="button">
+			        <button type="submit">Ajouter</button>
+			    </div>
+			</form>
+		</div>	
+
+		<br>
+
+		<div id="addBook" style="border-style: solid;">
+			<label>Ajouter un livre</label>
+			<form action="addBook" method="post">
+			    <div>
+			        <label for="titre">Titre :</label>
+			        <input type="text" id="titre" name="titre" />
+			    </div> 
+			    <div>
+			        <select name="author">
+						<?php
+						foreach($authors as $author) {
+							echo "<option value=".$author->getId()."----".$author->getLastName().">$author</option>";
+						}
+						?>
+					</select>
+					</select>
+					<select name="publisher">
+						<?php
+							foreach($publishers as $publisher) {
+							echo "<option value=".$publisher->getId()."----".$publisher->getName().">$publisher</option>";
+						}
+						?>
+					</select>
+			    </div>  
+			    <div class="button">
+			        <button type="submit">Ajouter</button>
+			    </div>
+			</form>
+		</div>
+
+	</body>
+</html>
